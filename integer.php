be783c7767fff6db5ab1db22984074be6031bf9e
AnDeriens <katsuya.ando496@gmail.com>
2021-01-14 03:55:46 +0900

    リファクタリング

--- integer.php
+++ integer.php
@@ -1,24 +1,105 @@
 <?php
+ini_set("display_errors", 0);
+ini_set("display_startup_errors", 0);
+ini_set('error_reporting', E_ALL);
+set_error_handler(function($errno, $errstr, $errfile, $errline){
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
 
-function to_int($v)
+require_once('./research_supporter.php');
+
+class Cast
 {
-    return (int)$v;
+    public static function int($v)
+    {
+        return (int)$v;    
+    }
 }
 
-var_dump(to_int(NULL));
-var_dump(to_int(0));
-var_dump(to_int(1));
-var_dump(to_int(''));
-var_dump(to_int('0'));
-var_dump(to_int('1'));
-var_dump(to_int('a'));
-var_dump(to_int(true));
-var_dump(to_int(false));
-var_dump(to_int(0.1));
-var_dump(to_int([]));
-var_dump(to_int(['a']));
-var_dump(to_int(['a', 'b']));
-var_dump(to_int([1, 2]));
-var_dump(to_int(new stdClass()));
-var_dump(to_int(new stdClass(['a' => 'a'])));
-var_dump(to_int(function () {return true;}));
+class Executer
+{
+    protected $exception_handler;
+
+    public function __construct()
+    {
+        $this->exception_handler = new ExceptionHandler(); 
+    }
+
+    public function execute($func, $value): string
+    {
+        return $this->handle_function_error($func, $value);
+    }
+
+    protected function handle_function_error(string $func, $value): string
+    {
+        ob_start();
+        try {
+            $r = call_user_func($func, $value);
+            var_dump($r);
+        } catch (\Exception $e) {
+            echo $this->exception_handler->handle($e);
+        }
+        return ob_get_clean();
+    }
+}
+
+class Reporter
+{
+    const NUM_START = 0;
+
+    protected $executer;
+    protected $values;
+    protected $results;
+    protected $num;
+
+    public function __construct()
+    {
+        $this->executer = new Executer; 
+        $this->num = self::NUM_START;
+    }
+
+    protected function increment()
+    {
+        $this->num++; 
+    }
+
+    public function addCase($method, $value)
+    {
+        $this->values[$this->num] = var_export($value, true);
+        $this->results[$this->num] = $this->executer->execute($method, $value);
+        $this->increment();
+    }
+
+    public function report()
+    {
+        echo "| value | result |\n";
+        echo "| --- | --- |\n";
+        for ($i = self::NUM_START; $i < $this->num; $i++) {
+            echo '| ' .  trim($this->values[$i]) . ' | ' . trim($this->results[$i]) . " |\n";
+        }
+    }
+}
+
+$reporter = new Reporter();
+
+$reporter->addCase("Cast::int", NULL);
+
+$reporter->report();
+
+// to_int(NULL);
+// to_int(0);
+// to_int(1);
+// to_int('');
+// to_int('0');
+// to_int('1');
+// to_int('a');
+// to_int(true);
+// to_int(false);
+// to_int(0.1);
+// to_int([]);
+// to_int(['a']);
+// to_int(['a', 'b']);
+// to_int([1, 2]);
+// to_int(new stdClass());
+// to_int(new stdClass(['a' => 'a']));
+// to_int(function () {return true;});
---
+++ research_supporter.php
@@ -0,0 +1,66 @@
+<?php
+
+class ExceptionHandler
+{
+    public function handle(\Exception $e)
+    {
+        if ($e instanceof ErrorException) {
+            switch ($e->getSeverity()) {
+                case  E_ERROR:
+                    $result = "E_ERROR";
+                    break;
+                case  E_WARNING:
+                    $result = "E_WARNING";
+                    break;
+                case  E_PARSE:
+                    $result = "E_PARSE";
+                    break;
+                case  E_NOTICE:
+                    $result = "E_NOTICE";
+                    break;
+                case  E_CORE_ERROR:
+                    $result = "E_CORE_ERROR";
+                    break;
+                case  E_CORE_WARNING:
+                    $result = "E_CORE_WARNING";
+                    break;
+                case  E_COMPILE_ERROR:
+                    $result = "E_COMPILE_ERROR";
+                    break;
+                case  E_COMPILE_WARNING:
+                    $result = "E_COMPILE_WARNING";
+                    break;
+                case  E_USER_ERROR:
+                    $result = "E_USER_ERROR";
+                    break;
+                case  E_USER_WARNING:
+                    $result = "E_USER_WARNING";
+                    break;
+                case  E_USER_NOTICE:
+                    $result = "E_USER_NOTICE";
+                    break;
+                case  E_STRICT:
+                    $result = "E_STRICT";
+                    break;
+                case  E_RECOVERABLE_ERROR:
+                    $result = "E_RECOVERABLE_ERROR";
+                    break;
+                case  E_DEPRECATED:
+                    $result = "E_DEPRECATED";
+                    break;
+                case  E_USER_DEPRECATED:
+                    $result = "E_USER_DEPRECATED";
+                    break;
+                default:
+                    $result = "E_UNKNOWN";
+                    break;
+            }
+        } else {
+            throw $e; 
+        }
+
+        return $result;
+    }
+}
+
+
